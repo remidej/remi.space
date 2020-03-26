4f073c22356d3c220d2e3dd7986e11034ba28a2d
Rémi de Juvigny <remidejuvigny@gmail.com>
2020-03-26 21:50:18 +0100

    Google Analytics setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,7 +55,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-62802408-1`,
       },
     },
     `gatsby-plugin-feed`,
