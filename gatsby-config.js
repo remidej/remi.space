152af29923464c860644f4dfc305083b9ed5c91e
Rémi de Juvigny <remidejuvigny@gmail.com>
2020-03-26 22:09:58 +0100

    Pre deploy fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,7 +55,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-62802408-1`,
       },
     },
     `gatsby-plugin-feed`,
@@ -68,7 +68,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/icon.png`,
       },
     },
     `gatsby-plugin-notify`,
