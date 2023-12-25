68f7350d885646491e94415962f7c239ef892817
Rémi de Juvigny <remi.dejuvigny@strapi.io>
2023-12-25 08:23:57 +0100

    add blog page

--- web/src/app/blog/page.tsx
+++ web/src/app/blog/page.tsx
@@ -1,3 +1,27 @@
-export default function BlogPage() {
-  return <h1>Blog Page</h1>;
+import { ArticlePreview } from "@/components/ArticlePreview";
+import type { APIResponseCollection } from "@/types/types";
+import { fetcher } from "@/utils/fetcher";
+
+export default async function BlogPage() {
+  const articles = (await fetcher("/api/articles", {
+    fields: ["title", "description", "slug"],
+    pagination: {
+      page: 1,
+      pageSize: 40,
+    },
+  })) as APIResponseCollection<"api::article.article">;
+
+  return (
+    <div className="container">
+      <h1 className="font-bold text-3xl font-semibold text-gray-800 dark:text-gray-300 mt-6">
+        All blog posts
+      </h1>
+      {articles.data.length === 0 && <p className="mt-8">No articles found</p>}
+      <section className="flex-1 mt-8">
+        {articles.data.map((article) => (
+          <ArticlePreview article={article} key={article.id} />
+        ))}
+      </section>
+    </div>
+  );
 }
