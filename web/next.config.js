9dfef8566dc1c1996b5a376953af70505e9d3a9b
Rémi de Juvigny <remi.dejuvigny@strapi.io>
2023-12-26 15:05:03 +0100

    hardcode domain

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost", process.env.VERCEL_URL],
+    domains: ["localhost", "remi.space"],
   },
 };
 
