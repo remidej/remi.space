e10b3197a78a520fdcd0495f02b928783f415053
Rémi de Juvigny <remi.dejuvigny@strapi.io>
2024-01-06 11:42:53 +0100

    remove blog page filters

--- web/src/app/blog/page.tsx
+++ web/src/app/blog/page.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import { ArticlePreview } from "@/components/ArticlePreview";
 import type { APIResponse, APIResponseCollection } from "@/types/types";
 import { fetcher } from "@/utils/fetcher";
-import { Filters } from "./Filters";
 import { URLSearchParams } from "url";
 import { type Metadata } from "next";
 
@@ -11,21 +10,10 @@ export default async function BlogPage({
 }: {
   searchParams: Record<string, string>;
 }) {
-  const tagsParam = searchParams.tags?.split(",").filter(Boolean) ?? [];
   const articles = await fetcher<APIResponseCollection<"api::article.article">>(
     "/api/articles",
     {
       fields: ["title", "description", "slug"],
-      populate: {
-        tags: {
-          fields: ["name", "slug"],
-        },
-      },
-      filters: {
-        title: {
-          $containsi: searchParams.search,
-        },
-      },
       sort: ["createdAt:desc"],
       pagination: {
         page: 1,
@@ -34,31 +22,13 @@ export default async function BlogPage({
     }
   );
 
-  const tags = await fetcher<APIResponseCollection<"api::tag.tag">>(
-    "/api/tags",
-    {
-      fields: ["name", "slug"],
-    }
-  );
-
-  const filteredArticles =
-    tagsParam.length > 0
-      ? articles.data.filter((article) => {
-          return article.attributes.tags?.data.some((tag) =>
-            tagsParam.includes(tag.attributes.slug!)
-          );
-        })
-      : articles.data;
-
   return (
-    <div className="container py-12">
-      <h1 className="text-3xl font-semibold text-gray-800 dark:text-gray-300">
+    <div className="container py-8">
+      <h1 className="uppercase tracking-wide text-blog-500 font-bold mb-6 text-lg">
         All blog posts
       </h1>
-      <Filters tags={tags} resultsCount={filteredArticles.length} />
-      {articles.data.length === 0 && <p className="mt-8">No articles found</p>}
-      <section className="flex-1 mt-8 flex flex-col gap-8">
-        {filteredArticles.map((article) => (
+      <section className="flex-1 flex flex-col gap-8">
+        {articles.data.map((article) => (
           <ArticlePreview article={article} key={article.id} />
         ))}
       </section>
