6a676e81590db97411072134f8716db04da64284
Rémi de Juvigny <remi.dejuvigny@strapi.io>
2024-01-09 00:30:26 +0100

    handle SITE_URL

--- web/src/utils/url.ts
+++ web/src/utils/url.ts
@@ -1,3 +1,5 @@
 const protocol = process.env.NODE_ENV === "production" ? "https" : "http";
+const hostname =
+  process.env.SITE_URL || process.env.VERCEL_URL || "localhost:3000";
 
-export const url = `${protocol}://${process.env.VERCEL_URL}`;
+export const url = `${protocol}://${hostname}`;
