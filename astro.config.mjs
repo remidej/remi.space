0682a01702cf504e9f9f71426e7c6bbd3228215f
Rémi de Juvigny <8087692+remidej@users.noreply.github.com>
2026-05-16 12:40:50 +0200

    set up cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   site: "https://remi.space",
   output: "static",
-  vite: { plugins: [tailwindcss()] },
+  vite: { plugins: [tailwindcss(), cloudflare()] },
   integrations: [sitemap()],
 });
